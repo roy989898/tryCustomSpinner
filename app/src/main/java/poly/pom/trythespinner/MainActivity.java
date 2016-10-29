561f4743472ec07d0bb8389cab3aff7dc8eb9903
roy989898 <roy989898@gmail.com>
2016-10-29 15:19:25 +0800

    can show the simple spinner list

--- app/src/main/java/poly/pom/trythespinner/MainActivity.java
+++ app/src/main/java/poly/pom/trythespinner/MainActivity.java
@@ -1,13 +1,32 @@
 package poly.pom.trythespinner;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import poly.pom.trythespinner.adapter.MyArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.sp_member)
+    Spinner spMember;
+    private String[] memberList = {"A", "B", "C"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        ArrayList<String> memberArrayList = new ArrayList<>(Arrays.asList(memberList));
+        MyArrayAdapter adapter = new MyArrayAdapter(this, memberArrayList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spMember.setAdapter(adapter);
+
     }
 }
